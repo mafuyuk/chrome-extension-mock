af4edbafc585eec728e4039261b31f4d3e5408f1
kamono-mafuyu <kamono-mafuyu@dmm.com>
2017-04-13 22:17:54 +0900

    fix: ES6化

--- js/embeded-script.js
+++ js/embeded-script.js
@@ -1,18 +1,19 @@
-window.MyEmbededProgram = {
-  helloWorld: function() {
-    let str  = 'Hello world!';
-    let link = '#';
-
+function addHelloTab() {
     if (document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax").length === 0) {
-      return;
+        return;
     }
 
-    let e = document.createElement('a');
-    e.setAttribute('class', 'js-selected-navigation-item reponav-item');
-    e.appendChild(document.createTextNode(str));
-    e.href = link;
-    document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax").item(0).appendChild(e);
-  }
-};
+    const str  = 'Hello';
+    const link = '#';
 
-MyEmbededProgram.helloWorld();
+    // 埋め込み用のaタグ作成
+    const aTag = document.createElement('a');
+    aTag.setAttribute('class', 'js-selected-navigation-item reponav-item');
+    aTag.appendChild(document.createTextNode(str));
+    aTag.href = link;
+
+    // Githubのタブに新しいタブ追加
+    document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax").item(0).appendChild(aTag);
+}
+
+addHelloTab();
