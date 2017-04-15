cc24f427d5682820393c217ed47241aad6da847e
mafuyuk <mafuyuk.m@gmail.com>
2017-04-15 13:16:04 +0900

    fix: typo

--- js/contentScript.js
+++ js/contentScript.js
@@ -2,7 +2,7 @@ function injectScript(filepath) {
   // body要素取得
   const body = document.getElementsByTagName('body')[0];
 
-  // bosyに追加するjsタグ用意
+  // bodyに追加するjsタグ用意
   const script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.setAttribute('src', chrome.extension.getURL(filepath));
