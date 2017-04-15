f9c918509c252b8b985d4be2c1d443c24737af59
mafuyuk <mafuyuk.m@gmail.com>
2017-04-15 13:10:48 +0900

    fix: リファクタ

--- js/embededScript.js
+++ js/embededScript.js
@@ -1,19 +1,16 @@
-function addHelloTab() {
-    if (document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax").length === 0) {
-        return;
-    }
+function addTab(name) {
+    if (document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax")) { return; }
 
-    const str  = 'Hello';
-    const link = '#';
+    const aTagProperty = {'str': name, 'link': '#'};
 
     // 埋め込み用のaタグ作成
     const aTag = document.createElement('a');
     aTag.setAttribute('class', 'js-selected-navigation-item reponav-item');
-    aTag.appendChild(document.createTextNode(str));
-    aTag.href = link;
+    aTag.appendChild(document.createTextNode(aTagProperty.str));
+    aTag.href = aTagProperty.link;
 
     // Githubのタブに新しいタブ追加
     document.getElementsByClassName("reponav js-repo-nav js-sidenav-container-pjax").item(0).appendChild(aTag);
 }
 
-addHelloTab();
+addTab('hello');
